98eb7c7101b2aee360dd5a5d0c7cec42759b9cec
gersonkaitor <gersonkaitor@gmail.com>
2023-04-08 16:41:28 +0800

    CRUD rest api

--- server.js
+++ server.js
@@ -3,36 +3,16 @@ const PORT = 5000;
 
 const app = express();
 
-const ideas = [
-  {
-    id: 1,
-    text: 'Positive NewsLetter, a newsletter that only shares positive, uplifting news',
-    tag: 'Technology',
-    username: 'TonyStark',
-    date: '2022-01-02',
-  },
-  {
-    id: 2,
-    text: 'Milk cartons that turn a different color the older that your milk is getting',
-    tag: 'Inventions',
-    username: 'SteveRogers',
-    date: '2022-01-02',
-  },
-  {
-    id: 3,
-    text: 'ATM location app which lets you know where the closest ATM is and if it is in service',
-    tag: 'Software',
-    username: 'BruceBanner',
-    date: '2022-01-02',
-  },
-];
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const ideasRouter = require('./routes/ideas');
 
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the RandomIdeas API' });
 });
 
-app.get('/api/ideas', (req, res) => {
-  res.json({ success: true, ideas });
-});
+app.use('/api/ideas', ideasRouter);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
