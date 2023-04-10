964b22a15bbb3083e370aebb0a3b582d67da358b
gersonkaitor <gersonkaitor@gmail.com>
2023-04-10 12:23:17 +0800

    IdeaList Component

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,12 @@
 import Modal from './components/Modal';
 import IdeaForm from './components/IdeaForm';
+import IdeaList from './components/IdeaList';
+
 import '@fortawesome/fontawesome-free/css/all.css';
 import './css/style.css';
 
 const modal = new Modal();
 const ideaForm = new IdeaForm();
+const ideaList = new IdeaList();
 ideaForm.render();
+ideaList.render();
